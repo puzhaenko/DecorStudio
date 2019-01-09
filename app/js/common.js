04e9c8c90b9f641886971f0750eb845017614286
Andrey Puzhaenko <Puzhaenko.kor@gmail.com>
2019-01-09 16:54:02 +0200

    Add slider functionality and some works with left sidebar

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,17 @@
 $(function() {
 
-	// Custom JS
+var elem = document.querySelector('#powerange_slider');
+var init = new Powerange(elem, { hideRange: true });
+
+
+var options = {
+  strings: ["Свадьбу", "День Рождения", "Юбилей", "Корпоратив"],
+  typeSpeed: 100,
+  smartBackspace: true, // this is a default
+  loop: true
+}
+
+var typed = new Typed(".typed_text", options);
+
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,7 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/powerange/dist/powerange.min.js',
+		'app/libs/typed.js/lib/typed.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
